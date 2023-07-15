c2de200b739509aaffef49b5beec86a609e5aaee
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-15 21:57:14 +0700

    Returning the view as "Index"

--- FPT/FPTBook/Controllers/HomeController.cs
+++ FPT/FPTBook/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace FPTBook.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
