19c7181c812a49610c127ddd64182d876fbd7921
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-16 20:07:42 +0700

    Set up ApplicationDbContext

---
+++ FPT/FPTBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FPTBook.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Snippet code to create constructor: ctor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                        
+        }
+    }
+}
