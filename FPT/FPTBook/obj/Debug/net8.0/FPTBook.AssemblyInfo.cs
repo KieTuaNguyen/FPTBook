1b682dedfe20299a204b84b558f47c8d982637ed
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-16 23:36:02 +0700

    Add Category Controller and corresponding view

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7de4c22f68c57511d1fae874df0a9649b7ac0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21bf41a5fd7accd4b2e631a5f50f94df95e0a551")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
