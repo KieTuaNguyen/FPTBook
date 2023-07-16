1aebb2011dc6ff56253d85643c17cbc757c001f9
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-16 23:58:34 +0700

    Seed Category Table

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b682dedfe20299a204b84b558f47c8d982637ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+766cea15da3e87ea5893deb45d8ca9bb5bc9c561")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
