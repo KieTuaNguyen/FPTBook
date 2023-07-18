592f42407d7ac8681af77b062b650989532b4976
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-18 10:51:38 +0700

    Client Side Validation

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911a2b6b826179917aa4787067c61430b5718666")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c6598439a0f27a4afdbfae8691d49b3340ad2ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
