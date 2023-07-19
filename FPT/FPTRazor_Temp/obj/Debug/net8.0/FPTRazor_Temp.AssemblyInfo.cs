daa1f0e8056c469ee5f9d46cdcbe0340a6badb48
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-19 23:02:52 +0700

    Setup EF Core

--- FPT/FPTRazor_Temp/obj/Debug/net8.0/FPTRazor_Temp.AssemblyInfo.cs
+++ FPT/FPTRazor_Temp/obj/Debug/net8.0/FPTRazor_Temp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTRazor_Temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e59f4c328fdcc7e2708d25eae269d0b32ac647ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab006ee13b637adcf19bb6e416215be4473e8ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTRazor_Temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTRazor_Temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
