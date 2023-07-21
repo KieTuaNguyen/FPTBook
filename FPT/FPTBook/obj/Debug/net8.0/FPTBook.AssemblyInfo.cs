eee414b1116db13903c3c978e3521f1df7d80360
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-21 10:48:06 +0700

    N-Tier Architecture

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c04602f9cd4cc06322327153f5a1fe93884581f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
