60007a7d018b595928aa512cf2b7c93ae52d79ae
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-22 22:55:00 +0700

    UnitOfWork in Action

--- FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
+++ FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9de08728176cb2cb48b6528cd7eb9167fb00700")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
