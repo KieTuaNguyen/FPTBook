cf5f1080e544aa283fd0f6135b78fa293f0d2622
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-22 23:27:11 +0700

    Dropdown in NavBar

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60007a7d018b595928aa512cf2b7c93ae52d79ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f320811e5ccca0dc680ff5b5f77c7a18f8cb132a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
