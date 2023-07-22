abed47e74bb7c0411f8338c3e03f4e57dbe50fc6
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-07-22 13:14:10 +0700

    IRepository Interface

--- FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
+++ FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f517fe6443ad5e0dc3a9341fe2fa9749f2e64e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
