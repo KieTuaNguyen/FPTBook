826039032af5676f81196b9dd1e85467d33a4de1
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-03 02:08:04 +0700

    Load DataTables

---
+++ FPT/FPTBook/wwwroot/js/product.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title', "width": "25%" },
+            { data: 'isbn', "width": "15%" },
+            { data: 'listPrice', "width": "10%" },
+            { data: 'author', "width": "20%" },
+            { data: 'category.name', "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group"></div>`
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}
