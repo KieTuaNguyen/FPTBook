b31d1d2bf58e0d0c20051aa76d671384f175b2d7
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-13 02:50:36 +0700

    Register an Application

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65eeb29a60884d2305080b26ff07e8da9f2c490")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0dc4198949e3c991941b88068ca4bc4202bcf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
