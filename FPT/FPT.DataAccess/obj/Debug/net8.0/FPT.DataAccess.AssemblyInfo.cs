c8ba7cd520eef9337fefe63c87e028851901d141
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-13 00:03:20 +0700

    Scaffold Identity

--- FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
+++ FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eee46e728c9fadd5320c1362bdf8cfcdbfad118")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93372bdf350dffddb63671b851e8b30b184d9fea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ad9e820ec1500f62137fd30471347ad6416fbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
