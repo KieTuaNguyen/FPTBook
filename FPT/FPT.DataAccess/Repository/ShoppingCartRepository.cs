ca5772aa116a807efc0515ab7057ccc592819c22
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-15 01:54:31 +0700

    Add Shopping Cart to Repository

---
+++ FPT/FPT.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,25 @@
+﻿using FPT.DataAccess.Repository.IRepository;
+using FPT.DataAcess.Data;
+using FPT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPT.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db) {
+            _db = db;
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
--- FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
+++ FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0eb42e17cbca9d714a4d5c251b6dcfcdbff4a05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6bece8ce9321cae3c6c5fadf8f76c377a439a16")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
