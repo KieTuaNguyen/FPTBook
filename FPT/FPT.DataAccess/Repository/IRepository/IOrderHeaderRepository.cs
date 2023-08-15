55b8c737bdaa9fd117b340bc1bf6abc08bfd4c64
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-15 23:24:09 +0700

    Add Order Header and Detail Repository

---
+++ FPT/FPT.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -0,0 +1,14 @@
+﻿using FPT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPT.DataAccess.Repository.IRepository
+{
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+    }
+}
--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d1e2ad2598bebfe89c8d196c232332e10e9ee1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e46ad370c4781b5e09ccf182d7307539cc7330")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
