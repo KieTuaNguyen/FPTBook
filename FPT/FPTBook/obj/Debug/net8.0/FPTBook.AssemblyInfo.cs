6ca1f1ed660f257aa0a9d4bc369adf8feed35b04
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-15 23:47:48 +0700

    Load Summary UI with data

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55aaa74a5ec136cdff0fda030b2252db4d0e2e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2912241a525131588a632210f59a7cf07a592d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
