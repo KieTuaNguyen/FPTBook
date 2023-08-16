91af255dc927ef154dfe8be4f09b76bf9034c00b
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-16 16:42:17 +0700

    Add Status Filter

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adfde57194e07000de4ee132aa7363d955af3b2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c73bc22cb07c430828f45fb9b6a5ac687691827c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
