0081758ef6a49257976f61e29c49a84524cdbea4
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-17 14:02:06 +0700

    Facebook Login in Action

--- FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
+++ FPT/FPTBook/obj/Debug/net8.0/FPTBook.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea72350c8f303e9838704125fdd2543ebf1996d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30e1690523957c5afd30f5592282f65119ccd30")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
