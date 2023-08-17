4a6e85427d2df7f43844192a29bcebdd4e8107b2
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-17 22:56:38 +0700

    IDbInitializer

--- FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
+++ FPT/FPT.DataAccess/obj/Debug/net8.0/FPT.DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45600713714c8dfff053b708fe070ba86c882def")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPT.DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
