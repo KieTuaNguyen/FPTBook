fefe22056c2e6fca7b53c311138c790b1ff29a9e
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-18 10:22:49 +0700

    [Debug] - Deployment

--- FPTBook/obj/Debug/net7.0/FPTBook.AssemblyInfo.cs
+++ FPTBook/obj/Debug/net7.0/FPTBook.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9a389b4a8e1ae9fffe0e09af291f84276733bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPTBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
