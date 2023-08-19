cace1adac2432ea78f6fd72a9b39a58e22e6756c
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2023-08-19 12:37:41 +0700

    Update Resource

--- FPT.Models/obj/Debug/net7.0/FPT.Models.AssemblyInfo.cs
+++ FPT.Models/obj/Debug/net7.0/FPT.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FPT.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96e9b18f01eb964f2b97516db6d4481ad02279c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FPT.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FPT.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
